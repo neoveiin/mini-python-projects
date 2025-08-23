53d72fdd2c3f7666d0da55ca2a67cb830607c6d4
NEOVEIIN <nnneov@gmail.com>
2025-08-23 15:09:38 +0530

    feat(main): implement fetch_posts function for scraping

--- Web-Scraping-In-Python/Save-Hacker-News-In-CSV/src/main.py
+++ Web-Scraping-In-Python/Save-Hacker-News-In-CSV/src/main.py
@@ -1,5 +1,28 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+HN_URL = "https://news.ycombinator.com/"
+EXPORT_FILENAME = "hn_top20.csv"
+
+
+def fetch_posts():
+    try:
+        response = requests.get(HN_URL, timeout=10)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error: Unable to fetch posts:\n{e}\n")
+        return []
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    return soup.find_all("span", class_="titleline")
+
+
+
 def main():
-    print("Hello from save-hacker-news-in-csv!")
+    pass
 
 
 if __name__ == "__main__":
