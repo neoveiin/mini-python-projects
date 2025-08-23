a383250c2db315615e77bc4e73c46d3ea32b4509
NEOVEIIN <nnneov@gmail.com>
2025-08-23 15:31:44 +0530

    feat(main): implement save_to_csv function and improve error message

--- Web-Scraping-In-Python/Save-Hacker-News-In-CSV/src/main.py
+++ Web-Scraping-In-Python/Save-Hacker-News-In-CSV/src/main.py
@@ -12,7 +12,7 @@ def fetch_posts():
         response = requests.get(HN_URL, timeout=10)
         response.raise_for_status()
     except requests.RequestException as e:
-        print(f"Error: Unable to fetch posts:\n{e}\n")
+        print(f"\nError: Unable to fetch posts:\n{e}\n")
         return []
     
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -20,9 +20,34 @@ def fetch_posts():
     return soup.select("span.titleline > a")
 
 
+def save_to_csv(posts):
+
+    if not posts:
+        print("No posts to save!")
+        return
+    
+    posts_to_save = []
+
+    for post in posts[:20]:
+        posts_to_save.append({
+            "url": post['href'],
+            "title": post.text
+        })
+    
+    with open(EXPORT_FILENAME, 'w', newline='', encoding="utf-8") as f:
+        wo = csv.DictWriter(f, fieldnames=["url", "title"])
+        wo.writeheader()
+
+        wo.writerows(posts_to_save)
+
+    print(F"Done! Posts from Hacker News are exported to '{EXPORT_FILENAME}'.")
+    return
+
 
 def main():
-    pass
+    print("Fetching posts...")
+    posts = fetch_posts()
+    save_to_csv(posts)
 
 
 if __name__ == "__main__":
